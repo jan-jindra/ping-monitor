37a94b64f51ad9ffda2ce7f4f91f701f3d4d3040
jan-jindra <46716071+jan-jindra@users.noreply.github.com>
2023-07-02 15:16:47 +0200

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/env bash
+
+path=$(realpath $0)
+scriptFolder=$(dirname $path)
+
 
 while :
 do
@@ -6,9 +10,7 @@ do
         now=$(date "+%d.%m.%C%y %H:%M:%S")
         echo
         echo "$now"
-        bash /root/run.sh
+        bash $scriptFolder/run.sh
         echo "Pres CRTL+C to quit..."
         sleep 5
 done
-
-
